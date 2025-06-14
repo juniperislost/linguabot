dc1731665a296dfca7a2c63d27af7e6ac4b5e0e4
juniperislost <juniper_is_lost@protonmail.com>
2025-06-14 01:00:07 -0500

    e

--- main.go
+++ main.go
@@ -2,88 +2,130 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
+	"math/rand"
 	"os"
-	"os/signal"
 	"strings"
-	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-type Config struct {
-	Token  string `json:"token"`
-	Prefix string `json:"prefix"`
-}
-
 var (
-	config Config
-	botID  string
+	config struct {
+		Token         string `json:"token"`
+		Prefix        string `json:"prefix"`
+		QOTDChannelID string `json:"qotd_channel_id"`
+		QOTDRoleID    string `json:"qotd_role_id"`
+	}
+	qotdQuotes []string
 )
 
 func main() {
-	if err := loadConfig("config.json"); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to load config: %v\n", err)
-		os.Exit(1)
-	}
+	loadConfig("config.json")
+	loadQuotes("quotes.json")
 
 	dg, err := discordgo.New("Bot " + config.Token)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to create Discord session: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("Failed to create Discord session: %v", err)
 	}
-
-	u, err := dg.User("@me")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to get bot user: %v\n", err)
-		os.Exit(1)
-	}
-	botID = u.ID
 
 	dg.AddHandler(messageCreate)
 
 	err = dg.Open()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to open Discord session: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("Failed to open Discord connection: %v", err)
 	}
 	defer dg.Close()
 
-	fmt.Println("LinguaBot is now running. Press CTRL-C to exit.")
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
-	<-stop
-}
+	log.Println("LinguaBot is now running!")
 
-func loadConfig(path string) error {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(data, &config)
+	go startQOTDAutoPost(dg)
+
+	select {}
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == botID {
+	if m.Author.Bot {
+		return
+	}
+	if len(m.Content) < len(config.Prefix) || m.Content[:len(config.Prefix)] != config.Prefix {
 		return
 	}
 
-	if !strings.HasPrefix(m.Content, config.Prefix) {
-		return
-	}
-
-	content := strings.TrimPrefix(m.Content, config.Prefix)
-	args := strings.Fields(content)
+	args := strings.Fields(m.Content[len(config.Prefix):])
 	if len(args) == 0 {
 		return
 	}
 
-	cmdName := strings.ToLower(args[0])
-	cmdArgs := args[1:]
+	cmd := strings.ToLower(args[0])
+	args = args[1:]
 
-	if handler, exists := CommandRegistry[cmdName]; exists {
-		handler(s, m, cmdArgs)
-	} else {
-		s.ChannelMessageSend(m.ChannelID, "Unknown command. Try `l!help`.")
+	if handler, ok := CommandRegistry[cmd]; ok {
+		handler(s, m, args)
+	}
+}
+
+func loadConfig(path string) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Failed to read config: %v", err)
+	}
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatalf("Failed to parse config: %v", err)
+	}
+}
+
+func loadQuotes(path string) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Failed to read quotes: %v", err)
+	}
+	err = json.Unmarshal(data, &qotdQuotes)
+	if err != nil {
+		log.Fatalf("Failed to parse quotes: %v", err)
+	}
+}
+
+func startQOTDAutoPost(s *discordgo.Session) {
+	postHour := 9
+	postMinute := 0
+
+	for {
+		now := time.Now().UTC()
+		nextPost := time.Date(now.Year(), now.Month(), now.Day(), postHour, postMinute, 0, 0, time.UTC)
+		if !nextPost.After(now) {
+			nextPost = nextPost.Add(24 * time.Hour)
+		}
+		time.Sleep(nextPost.Sub(now))
+
+		if len(qotdQuotes) == 0 || config.QOTDChannelID == "" {
+			continue
+		}
+
+		question := qotdQuotes[rand.Intn(len(qotdQuotes))]
+
+		content := ""
+		if config.QOTDRoleID != "" {
+			content = "<@&" + config.QOTDRoleID + ">"
+		}
+
+		embed := &discordgo.MessageEmbed{
+			Title:       "Question of the Day",
+			Description: question,
+			Color:       0xC8A2C8,
+			Timestamp:   time.Now().UTC().Format(time.RFC3339),
+		}
+
+		_, err := s.ChannelMessageSendComplex(config.QOTDChannelID, &discordgo.MessageSend{
+			Content: content,
+			Embed:   embed,
+		})
+		if err != nil {
+			log.Printf("Failed to send QOTD: %v", err)
+		}
+
+		time.Sleep(24 * time.Hour)
 	}
 }
